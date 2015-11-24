16f62b93c9f8163e218f019bf71ddeeb3009b6bf
Feng Shu <tumashu@163.com>
2015-11-24 13:55:02 +0800

    Use org-webpage to generate README.md

--- chinese-fonts-setup.el
+++ chinese-fonts-setup.el
@@ -1,5 +1,6 @@
 ;;; chinese-fonts-setup.el --- A fonts config tool enforcing double-width Chinese character display
 
+;; * Header
 ;; Copyright (c) 2011-2015, Feng Shu
 
 ;; Author: Feng Shu <tumashu@163.com>
@@ -14,12 +15,12 @@
 ;; modify it under the terms of the GNU General Public License
 ;; as published by the Free Software Foundation; either version 3
 ;; of the License, or (at your option) any later version.
-;;
+
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;;
+
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs; see the file COPYING.  If not, write to the
 ;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
@@ -27,95 +28,104 @@
 
 ;;; Commentary:
 
+;; * Chinese-fonts-setup README                                         :README:
 ;; `chinese-fonts-setup' 是一个emacs中文字体配置工具。可以比较方便地
 ;; 实现中文字体和英文字体等宽（也就是大家常说的中英文对齐）。
-;;
+
 ;; 这个package特别适用于需要处理中英文混合表格的中文org-mode用户。
-;;
-;; ### 过程展示 ###
-;;
+
+;; ** 过程展示
+
 ;;     http://www.tudou.com/programs/view/v7Kr0_a9INw/
-;;
-;; ### 下载 ###
-;;
+
+;; ** 下载
+
 ;;     https://github.com/tumashu/chinese-fonts-setup
-;;
-;; ### 安装 ###
+
+;; ** 安装
 ;; 1. 配置melpa源，参考：http://melpa.org/#/getting-started
 ;; 2. M-x package-install RET chinese-fonts-setup RET
 ;; 3. 在emacs配置文件中（比如: ~/.emacs）添加如下代码：
-;;
-;; ```lisp
+
+;; #+BEGIN_EXAMPLE
 ;; (require 'chinese-fonts-setup)
-;; ```
-;;
-;; ### 配置 ###
+;; #+END_EXAMPLE
+
+
+;; ** 配置
 ;; chinese-fonts-setup 使用profile的概念，来实现特定的环境使用特定的
 ;; 字体配置，比如：在编程时使用 “Consolas + 微米黑”，在阅读文章时使用
 ;; “PragmataPro + 黑体”。
-;;
+
 ;; 每一个profile都是一个emacs-lisp文件。其中包括了英文字体设置，中文字体设置
 ;; 以及中文字体大小。
 
 ;; chinese-fonts-setup 默认使用三个profile: profile1, profile2 和 profile3,
 ;; 如果想使用其他有意义的名称，可以使用下面类似的方式配置:
-;;
-;; ```lisp
+
+;; #+BEGIN_EXAMPLE
 ;; (setq cfs-profiles
 ;;     '("program" "org-mode" "read-book"))
-;; ```
-;;
+;; #+END_EXAMPLE
+
 ;; 所有的profile文件都保存在 `cfs-profiles-directory' 对应的目录中。
 ;; 如果profile文件不存在， `chinese-fonts-setup' 将使用其自带的fallback
 ;; 信息来配置字体。
-;;
+
 ;; 在运行profile编辑命令`cfs-edit-profile'的时候，缺失的falback文件
 ;; 将会自动创建，其原始内容为软件包自带的fallback信息。
-;;
+
 ;; `chinese-fonts-setup' 默认不会覆盖已经存在的profile文件。当需要重置
 ;; 某个profile文件时，可以使用命令：`cfs-regenerate-profile'。这个命令
 ;; 会强制覆盖profile文件，请做好备份。
-;;
+
 ;; 切换 profile 的命令有：
-;;
-;; 1. `cfs-switch-profile' (使用ido切换profile)
-;; 2. `cfs-next-profile'   (直接切换到下一个profile)
-;;
+
+;; | Command              | Help                    |
+;; |----------------------+-------------------------|
+;; | `cfs-switch-profile' | 使用ido切换profile      |
+;; | `cfs-next-profile'   | 直接切换到下一个profile |
+
 ;; 如果当前的profile不适合时，可以通过`cfs-edit-profile'来编辑当前
 ;; 的profile文件。chinese-fonts-setup自带一个profile-edit编辑模式。
-;;
-;; 1.  C-c C-c     `cfs-test-fontsizes-at-point'
-;;                  查看字体显示效果
-;; 2.  C-up        `cfs-increment-fontsize-at-point'
-;;                  增大光标下字号的大小，同时显示对齐效果
-;; 3.  C-down      `cfs-decrement-fontsize-at-point'
-;;                  减小光标下字号的大小，同时显示对齐效果
-;;
+
+;; | Key     | Command                           | Help                                   |
+;; |---------+-----------------------------------+----------------------------------------|
+;; | C-c C-c | `cfs-test-fontsizes-at-point'     | 查看字体显示效果                       |
+;; | C-up    | `cfs-increment-fontsize-at-point' | 增大光标下字号的大小，同时显示对齐效果 |
+;; | C-down  | `cfs-decrement-fontsize-at-point' | 减小光标下字号的大小，同时显示对齐效果 |
+
+
 ;; 配置完成后，有可能需要重启 Emacs。(参考： http://debbugs.gnu.org/db/17/1785.html)
-;;
-;; ### 调整字体大小 ###
+
+;; ** 调整字体大小
 ;; `chinese-fonts-setup' 使用下述两个命令调整字体大小:
-;;
-;; 1.  `cfs-increase-fontsize' 增大字体大小
-;; 2.  `cfs-decrease-fontsize' 减小字体大小
-;;
+
+;; | Command                 | Help         |
+;; |-------------------------+--------------|
+;; | `cfs-increase-fontsize' | 增大字体大小 |
+;; | `cfs-decrease-fontsize' | 减小字体大小 |
+
 ;; 在调整字体大小的同时，字号信息也通过customize-save-variable函数保存到~/.emacs中了。
-;;
-;; ### Tips ###
-;;
+
+;; ** Tips
+
 ;; 1. 使用命令: `describe-char' 可以了解光标处字符使用什么字体。
 ;; 2. 在 scratch 中写一行 elisp 代码： (cl-prettyprint (font-family-list)),
 ;;    执行后，就会在 scratch 中插入当前可用字体的名称列表，这是一个很有用的技巧。
 ;; 3. 命令：`cfs-insert-fontname', 可以让用户选择一个可用字体插入到当前光标处。
 ;; 4. Windows 用户 (特别是 Windows XP 用户) 可以安装 MacType 软件来优化
 ;;    字体显示效果，推荐使用。
-;;
-;; ### 参考文章 ###
-;;
+
+;; ** 参考文章
 ;; 1. http://baohaojun.github.io/perfect-emacs-chinese-font.html
 ;; 2. http://zhuoqiang.me/torture-emacs.html
 
+
 ;;; Code:
+
+;; * 代码                                                                 :code:
+;; #+BEGIN_SRC emacs-lisp
 (require 'cl-lib)
 (require 'ido)
 (require 'thingatpt)
@@ -637,7 +647,10 @@ The below is an example which is used to set symbol fonts:
     (when choose
       (insert (format "\"%s\"" choose)))))
 
+;; #+END_SRC
 
+;; * Footer
+;; #+BEGIN_SRC emacs-lisp
 (provide 'chinese-fonts-setup)
 
 ;; Local Variables:
@@ -645,3 +658,4 @@ The below is an example which is used to set symbol fonts:
 ;; End:
 
 ;;; chinese-fonts-setup.el ends here
+;; #+END_SRC
