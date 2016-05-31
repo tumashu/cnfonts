835e63dba5a02b7352b98f81620b658162863cca
Feng Shu <tumashu@163.com>
2016-05-31 09:17:20 +0800

    Update README

--- chinese-fonts-setup.el
+++ chinese-fonts-setup.el
@@ -1,4 +1,4 @@
-;;; chinese-fonts-setup.el --- A fonts config tool enforcing double-width Chinese character display
+;;; chinese-fonts-setup.el --- Emacs fonts config tool enforcing double-width Chinese character display
 
 ;; * Header
 ;; Copyright (c) 2011-2015, Feng Shu
@@ -28,19 +28,36 @@
 
 ;;; Commentary:
 
-;; * Chinese-fonts-setup 简要说明                                       :README:
-;; `chinese-fonts-setup' 是一个emacs中文字体配置工具。可以比较方便地
+;; * Chinese-fonts-setup README                                         :README:
+
+;; ** 简介
+;; Chinese-fonts-setup 是一个 emacs 中英文字体配置工具。可以比较方便地
 ;; 实现中文字体和英文字体等宽（也就是大家常说的中英文对齐）。
 
-;; 这个package特别适用于需要处理中英文混合表格的中文org-mode用户。
+;; 注： 这个 package 特别适用于需要处理中英文混合表格的中文 org-mode 用户。
+;; ** 基本原理
+;; Chinese-fonts-setup 的核心很简单，就是让中文字体和英文字体使用不同的字号，
+;; 从而实现中英文对齐，它下面的样例代码原理是一样的：
 
-;; ** 过程展示
-;; http://www.tudou.com/programs/view/v7Kr0_a9INw/
+;; #+BEGIN_EXAMPLE
+;; English Font
+;; (set-frame-font "-unknown-文泉驿等宽微米黑-normal-normal-normal-*-14-*-*-*-*-0-iso10646-1")
+;; Chinese Font
+;; (dolist (charset '(kana han symbol cjk-misc bopomofo))
+;;   (set-fontset-font (frame-parameter nil 'font)
+;;                     charset (font-spec :family "Microsoft Yahei" :size 16)))
+;; #+END_EXAMPLE
 
-;; ** 下载
-;; https://github.com/tumashu/chinese-fonts-setup
+;; ** 使用特点
+;; Chinese-fonts-setup 添加了许多辅助工具，使配置和调节字体和字号的工作更加简便和快捷，简单来说，
+;; 有下面几个优点：
 
-;; ** 安装
+;; 1. 安装即用：Chinese-fonts-setup 内置字体 fallback 功能，只需安装，就能够配置中文字体和英文字体，
+;;    让中文可以正确显示，不会因为 emacs 配置中设置的字体不存在而报错。
+;; 2. 设置方便：Chinese-fonts-setup 自带一个 profile 文件编辑模式，可以让用户动态的更换字体和调节字体
+;;    大小，分分钟实现用户指定的中文字体和英文字体的等宽对齐。
+
+;; ** 下载安装
 ;; 1. 配置melpa源，参考：http://melpa.org/#/getting-started
 ;; 2. M-x package-install RET chinese-fonts-setup RET
 ;; 3. 在emacs配置文件中（比如: ~/.emacs）添加如下代码：
@@ -49,42 +66,66 @@
 ;; (require 'chinese-fonts-setup)
 ;; #+END_EXAMPLE
 
-;; ** 配置
-;; chinese-fonts-setup 使用 profile 的概念，来实现特定的环境使用特定的
-;; 字体配置，比如：在编程时使用 “Consolas + 微米黑”，在阅读文章时使用
-;; “PragmataPro + 黑体”。
+;; ** 配置使用
+;; *** 编辑使用 profile
+;; 一个 profile 代表了一套字体配置，chinese-fonts-setup 使用 profile 的概念，
+;; 来维护多套字体配置，从而实现特定的环境使用特定的字体配置，
+;; 比如：在编程时使用 “Consolas + 微米黑”，在阅读文章时使用 “PragmataPro + 黑体”，等等。
 
-;; 每一个profile都是一个emacs-lisp文件。其中包括了英文字体设置，中文字体设置
-;; 以及中文字体大小。
+;; 在 `cfs-profiles-directory' 目录中, 每一个 profile 都对应一个 emacs-lisp 文件,
+;; 这些文件包含了英文字体设置，中文字体设置以及中文字体大小，类似：
 
-;; chinese-fonts-setup 默认使用三个profile: profile1, profile2 和 profile3,
-;; 如果想使用其他有意义的名称，可以使用下面类似的方式配置:
+;; #+BEGIN_EXAMPLE
+;;; `cfs--custom-set-fontsnames' 列表有3个子列表，第1个为英文字体列表，第2个为中文字体列表，
+;;; 第3个列表中的字体用于显示不常用汉字，每一个字体列表中，*第一个* *有效并可用* 的字体将被使用。
+;;; 将光标移动到上述列表中，按 `C-c C-c' 可以测试字体显示效果。另外，用户可以通过命令
+;;; `cfs-insert-fontname’ 来选择一个 *可用* 字体，然后在当前光标处插入其字体名称。
+;; (setq cfs--custom-set-fontnames
+;;       '(
+;;         ("PragmataPro" "Monaco" "Consolas" "DejaVu Sans Mono" "Droid Sans Mono" "Courier" "Courier New" "Liberation Mono" "Ubuntu Mono" "Droid Sans Mono Pro" "Inconsolata" "Source Code Pro" "Lucida Console" "Envy Code R" "Andale Mono" "Lucida Sans Typewriter" "monoOne" "Lucida Typewriter" "Panic Sans" "Hack" "Bitstream Vera Sans Mono" "HyperFont" "PT Mono" "Ti92Pluspc" "Excalibur Monospace" "Menlof" "Cousine" "Fira Mono" "Lekton" "M+ 1mn" "BPmono" "Free Mono" "Anonymous Pro" "ProFont" "ProFontWindows" "Latin Modern Mono" "Code 2002" "ProggyCleanTT" "ProggyTinyTT")
+;;         ("文泉驿等宽微米黑" "Noto Sans S Chinese Regular" "微软雅黑" "Microsoft Yahei" "Microsoft_Yahei" "文泉驿等宽微米黑" "文泉驿等宽正黑" "黑体" "Hiragino Sans GB" "文泉驿正黑" "文泉驿点阵正黑" "SimHei" "SimSun" "NSimSun" "FangSong" "KaiTi" "FangSong_GB2312" "KaiTi_GB2312" "LiSu" "YouYuan" "新宋体" "宋体" "楷体_GB2312" "仿宋_GB2312" "幼圆" "隶书" "STXihei" "STKaiti" "STSong" "STZhongsong" "STFangsong" "FZShuTi" "FZYaoti" "STCaiyun" "STHupo" "STLiti" "STXingkai" "STXinwei" "方正姚体" "方正舒体" "方正粗圆_GBK" "华文仿宋" "华文中宋" "华文彩云" "华文新魏" "华文细黑" "华文行楷")
+;;         ("HanaMinB")
+;;         ))
+
+;;; `cfs--custom-set-fontsizes' 中，所有元素的结构都类似：(英文字号 中文字号 EXT-B字体字号)
+;;; 将光标移动到各个数字上，按 C-c C-c 查看光标处字号的对齐效果。
+;;; 按 C-<up> 增大光标处字号，按 C-<down> 减小光标处字号。
+;; (setq cfs--custom-set-fontsizes
+;;       '(
+;;         (9    9.0 11.0)
+;;         (10   10.5 12.5)
+;;         (11.5 12.0 14.0)
+;;         (12.5 13.5 15.0)
+;;         (14   15.0 16.5)
+;;         (16   16.5 20.0)
+;;         (18   18.0 21.0)
+;;         (20   21.0 24.0)
+;;         (22   22.5 26.0)
+;;         ))
+;; #+END_EXAMPLE
+
+;; Chinese-fonts-setup 默认使用三个 profile: profile1, profile2 和 profile3,
+;; 如果想使用其它有意义的名称，可以设置:
 
 ;; #+BEGIN_EXAMPLE
 ;; (setq cfs-profiles
 ;;     '("program" "org-mode" "read-book"))
 ;; #+END_EXAMPLE
 
-;; 所有的profile文件都保存在 `cfs-profiles-directory' 对应的目录中。
-;; 如果profile文件不存在， `chinese-fonts-setup' 将使用其自带的fallback
-;; 信息来配置字体。
-
-;; 在运行profile编辑命令`cfs-edit-profile'的时候，缺失的falback文件
-;; 将会自动创建，其原始内容为软件包自带的fallback信息。
-
-;; `chinese-fonts-setup' 默认不会覆盖已经存在的profile文件。当需要重置
-;; 某个profile文件时，可以使用命令：`cfs-regenerate-profile'。这个命令
-;; 会强制覆盖profile文件，请做好备份。
-
-;; 切换 profile 的命令有：
+;; 用户可以使用下面两个命令快速切换 profile：
 
 ;; | Command            | Help                    |
 ;; |--------------------+-------------------------|
 ;; | cfs-switch-profile | 使用ido切换profile      |
 ;; | cfs-next-profile   | 直接切换到下一个profile |
 
-;; 如果当前的profile不适合时，可以通过`cfs-edit-profile'来编辑当前
-;; 的profile文件。chinese-fonts-setup自带一个profile-edit编辑模式。
+;; 如果用户觉得 *当前使用* 的 profile 不符合个人使用习惯时，可以使用 `cfs-edit-profile'
+;; 命令来编辑当前 profile 文件，如果对应的 profile 文件不存在，chinese-fonts-setup
+;; 会在编辑之前自动创建, *不需要用户手动创建 profile 文件* ，已存在的 profile 文件不会被覆盖，
+;; 用户可以使用命令：`cfs-regenerate-profile' 强制重置某个 profile 文件！
+
+;; `cfs-edit-profile' 命令会打开当前 profile 文件，并激活一个内置的 profile 编辑模式，
+;; 在编辑的过程中，可以使用下面的三个命令来 *快速的* 测试编辑效果：
 
 ;; | Key     | Command                         | Help                                   |
 ;; |---------+---------------------------------+----------------------------------------|
@@ -94,7 +135,11 @@
 
 ;; 配置完成后，有可能需要重启 Emacs。(参考： http://debbugs.gnu.org/db/17/1785.html)
 
-;; ** 调整字体大小
+;; [[./snapshots/cfs-edit-fontnames.gif]]
+
+;; [[./snapshots/cfs-edit-fontsizes.gif]]
+
+;; *** 调整字体大小
 ;; `chinese-fonts-setup' 使用下述两个命令调整字体大小:
 
 ;; | Command               | Help         |
@@ -102,7 +147,9 @@
 ;; | cfs-increase-fontsize | 增大字体大小 |
 ;; | cfs-decrease-fontsize | 减小字体大小 |
 
-;; 在调整字体大小的同时，字号信息也通过customize-save-variable函数保存到~/.emacs中了。
+;; 注意：在调整字体大小的同时，字号信息也会保存 ~/.emacs 中。
+
+;; [[./snapshots/cfs-increase-and-decrease-fontsize.gif]]
 
 ;; ** Tips
 
