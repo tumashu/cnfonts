4fef30736a3338bb3fa1eb476d24d8f059dc724f
Feng Shu <tumashu@163.com>
2016-06-02 11:09:15 +0800

    cfs--read-profile, print meanful load message

--- chinese-fonts-setup.el
+++ chinese-fonts-setup.el
@@ -351,7 +351,8 @@ The below is an example which is used to set symbol fonts:
   (interactive)
   (let ((file (cfs--get-current-profile)))
     (if (file-readable-p file)
-        (progn (load-file file)
+        (progn (load (expand-file-name file) nil t)
+               (message "Load chinese-fonts-setup profile: %S." cfs--current-profile)
                (list
                 (if (boundp 'cfs--custom-set-fontnames)
                     cfs--custom-set-fontnames
