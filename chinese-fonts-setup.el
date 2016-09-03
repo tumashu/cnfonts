b818282b42363704a4912a2e8d9ff294c027e69a
Feng Shu <tumashu@163.com>
2016-09-03 08:54:52 +0800

    Update README

--- chinese-fonts-setup.el
+++ chinese-fonts-setup.el
@@ -146,8 +146,8 @@
 ;; | C-up    | cfs-increment-fontsize-at-point | 增大光标下字号的大小，同时显示对齐效果 |
 ;; | C-down  | cfs-decrement-fontsize-at-point | 减小光标下字号的大小，同时显示对齐效果 |
 
-;; 注：1. 不建议 chinese-fonts-setup 新用户使用这种方式
-;;     2. 配置完成后，有可能需要重启 Emacs, 参考：http://debbugs.gnu.org/db/17/1785.html
+;; 注1: 不建议 chinese-fonts-setup 新用户使用这种方式
+;; 注2: 配置完成后，有可能需要重启 Emacs, 参考：http://debbugs.gnu.org/db/17/1785.html
 
 ;; *** 使用 cfs-regenerate-profile 重置 profile
 ;; `cfs-regenerate-profile' 命令会使用 chinese-fonts-setup 自带的
