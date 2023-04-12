5115f53366bd6118dce3673ddec5ff428534ce67
Feng Shu <tumashu@163.com>
2023-04-12 09:32:53 +0800

    v1.1.1

--- cnfonts.el
+++ cnfonts.el
@@ -6,7 +6,7 @@
 ;; Author: Feng Shu <tumashu@163.com>
 ;; URL: https://github.com/tumashu/cnfonts
 ;; Package-Requires: ((emacs "24"))
-;; Version: 1.1.0
+;; Version: 1.1.1
 ;; Keywords: convenience, Chinese, font
 
 ;; This file is not part of GNU Emacs.
